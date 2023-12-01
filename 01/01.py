7ff821213c61e432f9dc2e73ad2aa8c56db83699
Dale Hagglund <dale.hagglund@gmail.com>
2023-11-30 20:06:05 -0700

    01: setup before question opens
    - 01.py: copied from last year's day 25 code, stripped to support
      boilerplate
    - test_it.py: initial pytest file

---
+++ 01/01.py
@@ -0,0 +1,139 @@
+import sys
+import typing as ty
+from dataclasses import dataclass, field
+from enum import Enum
+import functools
+from functools import partial
+import collections
+import re
+import itertools
+from itertools import (
+    islice,
+    product,
+    pairwise,
+    zip_longest
+)
+import abc
+from functools import reduce, cmp_to_key
+import operator
+import collections
+from copy import deepcopy
+import unittest
+
+def star(f):
+    return lambda t: f(*t)
+
+def splitby(predicate, items):
+    batch = []
+    for item in items:
+        if predicate(item):
+            yield batch
+            batch = []
+            continue
+        else:
+            batch.append(item)
+    if batch:
+        yield batch
+        
+def chunked(items, n):
+    iters = [ iter(items) ] * n
+    return zip(*iters, strict=True)
+
+def sliding_window(items, n):
+    window = collections.deque(maxsize=n)
+    window.extend(islice(items, n))
+    if len(window) == n:
+        yield tuple(window)
+    for item in items:
+        window.append(item)
+        yield tuple(window)
+    
+def trim_newline(line: str) -> str:
+    return line[:-1] if line[-1] == '\n' else line
+        
+def read_sections(file, trim=True) -> list[list[str]]:
+    s = iter(file)
+    s = map(trim_newline, s)
+    s = map(str.rstrip if trim else ident, s)
+    s = splitby(lambda line: line == '', s)
+    return list(s)
+    
+def convert_fields(funcs, items: ty.Sequence[ty.Any]):
+    return tuple(
+        f(item)
+        for f, item
+        in zip(funcs, items)
+        if f is not None
+    )
+
+def ident(x): return x
+    
+def observe(func, items):
+    for item in items:
+        func(item)
+        yield item
+
+def drain(iterable):
+    for s in iterable:
+        pass
+
+def collect(factory, iterable):
+    return factory(iterable)
+    
+def takeuntil(predicate, items):
+    for item in items:
+        yield item
+        if predicate(item):
+            break
+
+def sign(n: int) -> int:
+    if   n < 0: return -1
+    elif n > 0: return +1
+    else:       return 0
+
+def convert_ints(items: list[str]) -> list:
+    return [
+        int(item) if re.match("^[0-9]*$", item) else item
+        for item in items
+    ]
+    
+def product(*ranges, repeat=1):
+    ranges = (
+        range(r) if isinstance(r, int) else r
+        for r in ranges
+    )
+    return itertools.product(*ranges, repeat=repeat)
+
+def nth(n, items): return items[n]
+def mul(items): reduce(operator.mul, items, 1)
+def seq(*funcs):
+    def _inner(arg):
+        for f in funcs:
+            arg = f(arg)
+        return arg
+    return _inner
+def compose(*funcs):
+    return seq(funcs[::-1])
+
+# map(seq(partial(map, f), list), ...)
+# map(compose(list, partial(map, f)), ...)
+# s = map(partial(map, f), ...); s = map(list, s)
+# map(lambda item: list(map(f, item)), ...)
+def mapinner(f, items):
+    for item in items:
+        yield list(map(f, item))
+
+def part1(fname: str):
+    with open(fname) as f:
+        sections = read_sections(f)
+    print(f'*** part 1 ***')
+
+def part2(fname: str):
+    with open(fname) as f:
+        sections = read_sections(f)
+    print(f'*** part 2 ***')
+
+if __name__ == '__main__':
+    part1(sys.argv[1])
+    part2(sys.argv[1])
+    
