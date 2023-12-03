55e904b21b976bd07eedf36702811b5c7752b22d
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-02 22:47:40 -0700

    03: solution for part 2

--- 03/03.py
+++ 03/03.py
@@ -24,25 +24,42 @@ def part1(fname: str):
     nrow = len(lines)
     ncol = len(lines[0])
     
-    
     non_symbols = set("0123456789.")    
-    def adjacent_to_sym(row, start, end):
+
+    def adjacent_syms(row, start, end):
+        syms = set()
         for col in range(start, end):
             for drow, dcol in product((-1, 0, 1), (-1, 0, 1)):
                 r, c = row + drow, col + dcol
                 if r < 0 or r >= nrow: continue
                 if c < 0 or c >= ncol: continue
                 if lines[r][c] not in non_symbols:
-                    return True
-        return False
+                    syms.add((r, c, lines[r][c]))
+        return syms
         
-    total = 0
+    parts = []
+    gears = collections.defaultdict(lambda: [])
     for row, line in enumerate(lines):
         for m in re.finditer(r"\d+", line):
-            if adjacent_to_sym(row, m.start(0), m.end(0)):
-                total += int(m.group(0))
+            adjacent = adjacent_syms(row, m.start(0), m.end(0))
+            if len(adjacent) == 0:
+                continue
+            partnum = int(m.group(0))
+            if len(adjacent) > 0:
+                parts.append(partnum)
+            for r, c, symbol in adjacent:
+                if symbol != "*": continue
+                gears[(r, c)].append(partnum)
 
-    print('part sum', total)
+    print('part sum', sum(parts))
+    print('ratio sum',
+        sum(
+            mul(parts)
+            for gear, parts
+            in gears.items()
+            if len(parts) == 2
+        )
+    )
     
 def part2(fname: str):
     with open(fname) as f:
