fa56fa897930d473f418b9554d35d197d6c1a3ad
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-05 20:26:38 -0700

    05: part 2 input parsing: initial version
    - this code appears to be parsing the ex1.txt into the new Map format
      properly.
    - it's creating the gap (delta=0) indices properly, which means that
      the code to expand an interval shouldn't have any special cases

--- 05/05-part2.py
+++ 05/05-part2.py
@@ -1,5 +1,6 @@
 import sys
 import typing as ty
+import dataclasses
 from dataclasses import dataclass, field
 from enum import Enum
 import collections
@@ -14,115 +15,107 @@ from functools import (
     partial,
     reduce
 )
+import pprint
 
 from tools import *
 
-Range = tuple[int, int]
+pp = pprint.PrettyPrinter(indent=2, sort_dicts=True)
+
+@dataclass(frozen=True)
+class Interval:
+    lo: int
+    hi: int
+    
+    def __post_init__(self):
+        if not(self.lo <= self.hi):
+            raise ValueError("Interval: 'lo' larger than 'hi'")
 
 @dataclass
 class Map:
-    source: str
-    dest: str
-    ranges: dict[Range, Range]
-    indices: list[Range]
-    
-    def __init__(self, lines):
+    _source: str
+    _dest: str
+    _shift: dict[Interval, int]
+    _indices: list[Interval]
+        
+    def source(self): return self._source
+    def dest(self): return self._dest
+
+    @classmethod
+    def from_section(cls, lines) -> ty.Self:
         title, *rest = lines
-        self.source, _, self.dest, *_ = re.split(r"[- ]", title)
+        source, _, dest, *_ = re.split(r"[- ]", title)
+
+        minint, maxint = 0, 2**33
+        
+        def in_range(n: int) -> bool:
+            return minint <= n <= maxint
+        def assert_in_range(n: int):
+           if not in_range(n):
+               raise ValueError(f'{n = } not in interval [{minint}, {maxint})')
+        def assert_valid_triple(dst_start: int, src_start: int, n: int):
+            assert n > 0, "expecting the mapped interval to be non-empty"
+            for i in (dst_start, src_start, n):
+                assert_in_range(i)
+            assert_in_range(dst_start + n)
+            assert_in_range(src_start + n)
+
         s = iter(rest)
         s = map(str.split, s)
         s = map(partial(map, int), s)
         s = map(tuple, s)
-        self.ranges = dict(
-            ((src_start, src_start + n), (dst_start, dst_start + n))
-            for dst_start, src_start, n
-            in s
-        )
-        self.indices = sorted(self.ranges.keys())
+        s = observe(partial(print, "#1"), s)
+        s = observe(star(assert_valid_triple), s)         
+        s = sorted(s, key=partial(nth, 1))
+        
+        shift = {}
+        indices = []
 
-    def translate(self, i):
-        #print(f'> translate: {i = }')
-        for slo, shi in self.indices:
-            if not (slo <= i < shi): continue
-            delta = i - slo
-            #print(f'> translate: found {(slo, shi) = } {delta = }')
-            dlo, dhi = self.ranges[(slo, shi)]
-            return dlo + delta
-        else:
-            return i
-            
-    def _find_position(self, n):
-        for i, (lo, hi) in enumerate(self.indices):
-            if n < hi: return i
-        else:
-            return len(self.indices)
-            
-    def translate_ranges(self, rset: set[Range]) -> set[Range]:
-        result = set()
-        for r in rset:
-            result.update(self.translate_range(r))
-        return result
+        def add_index(interval: Interval, delta):
+            indices.append(interval)
+            shift[interval] = delta
 
-    def translate_range(self, r: Range) -> set[Range]:
-        print(f'> Map.translate_range({self.source}, {self.dest}): {r = }')
-        def intersect(r1: Range, r2: Range) -> ty.Optional[Range]:
-            r1, r2 = (r1, r2) if r1[0] <= r2[0] else (r2, r1)
-            r1lo, r1hi = r1
-            r2lo, r2hi = r2
-            assert r1lo <= r2lo
-
-            if r2lo >= r1hi: return None
-            interval = ( max(r1lo, r2lo), min(r1hi, r2hi) )
-            if interval[0] == interval[1]: return None
-            return interval
+        for curtuple in s:
+            curdst, cursrc, n = curtuple
+            delta = curdst - cursrc
+            cur = Interval(cursrc, cursrc + n)
             
-        def adjust_intersection(index, r: Range) -> Range:
-            delta = self.ranges[index][0] - index[0]
-            return (r[0] + delta, r[1] + delta)
+            if len(indices) == 0 and cur.lo == minint:
+                add_index(cur, delta)
+                continue
+                
+            if len(indices) == 0 and cur.lo > minint:
+                add_index(Interval(minint, cur.lo), 0)
+                
+            if indices[-1].hi < cur.lo:
+                gap = Interval(indices[-1].hi, cur.lo)
+                add_index(gap, 0)
+            add_index(cur, delta)
             
-        s = iter(self.indices)
-        s = map(lambda index: (index, intersect(r, index)), s)
-        s = filter(star(lambda _, intersection: intersection is not None), s)
-        s = map(star(adjust_intersection), s)
-        #s = sorted(s)
-        #return merge_intervals(s)            
-        return set(s)  
+        add_index(Interval(indices[-1].hi, maxint), 0)
+        
+        print("#10: source", pp.pformat(source))
+        print("#10: dest  ", pp.pformat(dest))
+        print("#10: shift ", pp.pformat(shift))
+        print("#10: indices", pp.pformat(indices))
+                
+        return cls(source, dest, shift, indices)        
     
 def parse_sections(sections: list[list[str]]):
     initial_seeds = list(map(int, re.split(r"[- ]", sections[0][0])[1:]))
 
     maps = dict()
     for sec in sections[1:]:
-        m = Map(sec)
-        maps[m.source] = m
+        m = Map.from_section(sec)
+        maps[m.source()] = m
         
     assert len(maps) == len(sections[1:])
     return initial_seeds, maps
- 
-def solve1(sections: list[list[str]]) -> int:
-    from pprint import pprint
-    
-    initial_seeds, maps = parse_sections(sections)
-
-    def translate(source, dest, value):
-        m = maps[source]
-        while m.dest != dest:
-            value = m.translate(value)
-            m = maps[m.dest]
-        return m.translate(value)
-
-    s = map(partial(translate, 'seed', 'location'), initial_seeds)
-    return min(s)
-    
-def part1(fname: str):
-    with open(fname) as f:
-        sections = read_sections(f)
-    print(f'*** part 1 ***', solve1(sections))
     
 def solve2(sections: list[list[str]]) -> int:
     from pprint import pprint
     
     initial_seeds, maps = parse_sections(sections)
+
 
     s = iter(initial_seeds)
     s = chunked(s, 2)
@@ -130,25 +123,12 @@ def solve2(sections: list[list[str]]) -> int:
     s = observe(print, s)
     s = map(star(lambda lo, n: (lo, lo + n)), s) # half-open intervals
     seed_ranges = set(s)
+
+    print('> initial_seeds ', pp.pformat(initial_seeds))
+    print('> seed ranges ', pp.pformat(seed_ranges))
+    print('> maps ', pp.pformat(maps))
     
-    pprint(seed_ranges)
-    
-    def expand(source, dest, ranges):
-        print('> expand', source, dest)
-        
-        m = maps[source]
-        while m.dest != dest:
-            print(f'   > {m.dest = } {ranges = }')
-            ranges = m.translate_ranges(ranges)
-            m = maps[m.dest]
-        ranges = m.translate_ranges(ranges)
-        return m.translate_ranges(ranges)
-
-    location_ranges = expand('seed', 'location', seed_ranges)
-
-    pprint(location_ranges)
-
-    return min(r[0] for r in location_ranges)
+    return -1
 
 def part2(fname: str):
     with open(fname) as f:
@@ -156,5 +136,5 @@ def part2(fname: str):
     print(f'*** part 2 ***', solve2(sections))
 
 if __name__ == '__main__':
-    part1(sys.argv[1])
+    #part1(sys.argv[1])
     part2(sys.argv[1])
