295304be0e6771311d70415becb0e9018c7b85b1
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-05 22:39:34 -0700

    06: solution for part 2
    - I was a bit surprised that the exhaustive search finished in a
      relatively short amount of time.
    - But, it would no doubt have taken much longer without the realization
      that you only need to find the lowest hold time _h_ for which the
      distance exceeds the required record, and from that you can directly
      compute the wins.
    - If this had been too slow, I was looking at either solving the
      quadratic inequality, or else doing a binary search for h from 0 to
      maxtime over 2.

--- 06/06.py
+++ 06/06.py
@@ -17,16 +17,21 @@ from functools import (
 
 from tools import *
 
-def read_input_tuples(sec: list[str]) -> list[tuple[int, int]]:
-    times = sec[0]
-    distances = sec[1]
+def read_input_tuples(sec: list[str], part2=False) -> list[tuple[int, int]]:
+    times = sec[0].split()[1:]
+    distances = sec[1].split()[1:]
     
-    return list(
-        zip(
-            map(int, times.split()[1:]),
-            map(int, distances.split()[1:])
+    if not part2:    
+        return list(
+            zip(
+                map(int, times),
+                map(int, distances)
+            )
         )
-    )
+        
+    time = int(''.join(times))
+    distance = int(''.join(distances))
+    return [ (time, distance) ]
 
 def dist(h: int, M: int) -> int:
     assert h <= M
@@ -47,6 +52,22 @@ def solve1(sec: list[str]) -> int:
                 product *= wins
                 break
     return product
+    
+def solve2(sec: list[str]) -> int:
+    games = read_input_tuples(sec, part2=True)
+    product = 1
+    for g in games:
+        M, record = g
+        print(f'> {M = } {record = }')
+        for h in range(0, M+1):
+            print(f'   > {h = } {dist(h, M) = }')
+            if dist(h, M) > record:
+                wins = (M - h) - h + 1
+                print(f'   > {wins = }')
+                product *= wins
+                break
+
+    return product
 
 def part1(fname: str):
     with open(fname) as f:
@@ -56,7 +77,7 @@ def part1(fname: str):
 def part2(fname: str):
     with open(fname) as f:
         sections = read_sections(f)
-    print(f'*** part 2 ***')
+    print(f'*** part 2 ***', solve2(sections[0]))
 
 if __name__ == '__main__':
     part1(sys.argv[1])
