86f84861710271f94bb63bf284b284427baa7529
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-08 01:14:02 -0700

    07: hand less than operation

--- 07/camel.py
+++ 07/camel.py
@@ -5,8 +5,12 @@ from functools import total_ordering
 CARDS = "23456789TJQKA"
 _CARD_ORDER = { card: pos for pos, card in enumerate(CARDS) }
 
+def card_pos(c: str) -> int:
+    assert len(c) == 1
+    return _CARD_ORDER[c]
+
 def cmp_card(c1: str, c2: str) -> int:
-    delta = _CARD_ORDER[c1] - _CARD_ORDER[c2]
+    delta = card_pos(c1) - card_pos(c2)
     if delta < 0: return -1
     if delta > 0: return +1
     return 0
@@ -17,6 +21,16 @@ class Hand:
     _counts: Counter[str]
     _labels: set[str]
     _shape: tuple[int, int, int, int, int]
+    
+    _SHAPE_RANK = {
+        (1, 1, 1, 1, 1): 0,
+        (1, 1, 1, 2): 1,
+        (1, 2, 2): 2,
+        (1, 1, 3): 3,
+        (2, 3): 4,
+        (1, 4): 5,
+        (5,): 6,
+    }
 
     def __init__(self, cards: str):
         assert len(cards) == 5
@@ -48,9 +62,13 @@ class Hand:
         
     def __eq__(self, other) -> bool:
         if self is other: return True
-        if self._counts != other._counts: return False
+        if self._shape != other._shape: return False
         return self._cards == other._cards
         
     def __lt__(self, other) -> bool:
-        if self is other: return False
-        
+        if self is other:
+            return False
+        if self._shape != other._shape:
+            print("> #2: ", self._shape, other._shape) 
+            return self._SHAPE_RANK[self._shape] < self._SHAPE_RANK[other._shape]        
+        return tuple(map(card_pos, self._cards)) < tuple(map(card_pos, other._cards))
--- 07/test_camel.py
+++ 07/test_camel.py
@@ -117,3 +117,34 @@ def test_one_pair(cards, expected):
 )
 def test_hand_equality(h1, h2, expected):
     assert (Hand(h1) == Hand(h2)) == expected
+
+@pytest.mark.parametrize(
+    "cards1,cards2,expected", [
+        ("23456", "2K456", True),
+        ("2K456", "23456", False),
+        ("23245", "2K245", True),
+        ("T5T44", "55344", False),
+        ("33344", "44333", True),
+        ("44333", "33344", False),
+    ]
+)
+def test_less_than_same_shape(cards1, cards2, expected):
+    h1, h2 = Hand(cards1), Hand(cards2)
+    assert h1._shape == h2._shape
+    assert (h1 < h2) == expected
+
+@pytest.mark.parametrize(
+    "cards1,cards2,expected", [
+        ("45678", "22456", True),
+        ("22456", "22336", True),
+        ("22336", "12333", True),
+        ("22256", "22233", True),
+        ("22233", "22226", True),
+        ("22226", "22222", True),
+    ]
+)
+def test_hand_less_than_different_shapes(cards1, cards2, expected):
+    h1, h2 = Hand(cards1), Hand(cards2)
+    assert h1._shape != h2._shape
+    assert (h1 < h2) == expected
+    assert (h1 > h2) == (not expected)
