f56ababa0f267aa5e9ec08052ae816881cfd22f4
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-08 03:20:02 -0700

    08: solution for part 2
    - I ended up finding a hint for this one.
    - The observation, which I didn't study the full input long enough to
      make, is that each starting node maps to a unique ending node, *and*
      that ending node cycles back to form a loop.
    - So, you can independently compute the steps needed for each starting
      node, and the take the LCM of those values to get the (very large)
      number where all the cycles would converge.
    
    To figure that idea out properly would have required
    
    - observing that the paths from the start nodes never intersected, ie,
      each end node is unique.
    
    - noticing that the end node loops back to the the node *just* after
      it's corresponding initial node. This means that each time you get
      back to that end node, you've taken the same number of steps.
    
    - and then noticing that the least common multiple of the step lenghts
      is when the naive algorithm would finally get a collection of all
      final nodes.
    
    One final note: you can go so far as to *check* each if each path would
    lead to a cycle by using Floyd's algorithm (aka, the tortoise-and-hare
    algorithm) for detecting loops in a linked list.

--- 08/08.py
+++ 08/08.py
@@ -14,11 +14,68 @@ from functools import (
     partial,
     reduce,
 )
+import numpy as np
+from pprint import pprint
 
 from tools import *
 
+def parse_input(sections):
+    directions = itertools.cycle(sections[0][0])
+    
+    s = iter(sections[1])
+    s = map(partial(re.split, "[ =(),]+"), s)
+    network = {
+        node: (left, right)
+        for node, left, right, _
+        in s
+    }
+
+    return directions, network
+
+def follow(net, dirs, start, final):
+    print(f'{start = }')
+    steps = 0
+    node = start
+    while not final(node):
+        steps += 1
+        dir = next(dirs)
+        node = net[node]["LR".index(dir)]
+    print(f'final = {node}')
+
+    return steps
+    
 def solve1(sections) -> int:
-    pass
+    directions, network = parse_input(sections)
+    
+    s = iter(sections[1])
+    s = map(partial(re.split, "[ =(),]+"), s)
+    network = {
+        node: (left, right)
+        for node, left, right, _
+        in s
+    }
+    
+    return follow(
+        network, directions,
+        'AAA',
+        lambda n: n == 'ZZZ'
+    )
+    
+def solve2(sections) -> int:
+    import math
+    directions, network = parse_input(sections)
+    
+    def initial(n: str): return n.endswith('A')
+    def final(n: str): return n.endswith('Z')
+    
+    nodes = [ n for n in network.keys() if initial(n) ]
+    steps = [ 
+        follow(network, directions, n, final)
+        for n
+        in nodes
+    ]
+    print(steps)
+    return math.lcm(*steps)
 
 def part1(fname: str):
     with open(fname) as f:
@@ -28,7 +85,7 @@ def part1(fname: str):
 def part2(fname: str):
     with open(fname) as f:
         sections = read_sections(f)
-    print(f'*** part 2 ***')
+    print(f'*** part 2 ***', solve2(sections))
 
 if __name__ == '__main__':
     part1(sys.argv[1])
