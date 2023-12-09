96028293fbd81f693a3157b759afee59c8df4fce
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-08 22:17:32 -0700

    09: solution for part 2

--- 09/09.py
+++ 09/09.py
@@ -22,9 +22,14 @@ from tools import *
 def differences(vec: list[int]) -> list[int]:
     return [ n2 - n1 for n1, n2 in pairwise(vec) ]
 
-def extrapolate(vec: list[int]) -> int:
+def extrapolate_forward(vec: list[int]) -> int:
     if all(n == 0 for n in vec): return 0
-    return vec[-1] + extrapolate(differences(vec))
+    return vec[-1] + extrapolate_forward(differences(vec))
+
+def extrapolate_backward(vec: list[int]) -> int:
+    if all(n == 0 for n in vec): return 0
+    return vec[0] - extrapolate_backward(differences(vec))
+
 
 def solve1(sections: list[list[str]]) -> int:
     s = iter(sections[0])
@@ -32,12 +37,19 @@ def solve1(sections: list[list[str]]) -> int:
     s = map(partial(map, int), s)
     s = map(list, s)
     s = observe(partial(print, "#1 "), s)
-    s = map(extrapolate, s)
+    s = map(extrapolate_forward, s)
     s = observe(partial(print, "#2 "), s)
     return sum(s)
 
 def solve2(sections: list[list[str]]) -> int:
-    return -1
+    s = iter(sections[0])
+    s = map(str.split, s)
+    s = map(partial(map, int), s)
+    s = map(list, s)
+    s = observe(partial(print, "#1 "), s)
+    s = map(extrapolate_backward, s)
+    s = observe(partial(print, "#2 "), s)
+    return sum(s)
     
 def part1(fname: str):
     with open(fname) as f:
