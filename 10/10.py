9ba44ca0cf9c3b6b538b81c47e36acd822abaca5
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-09 00:33:04 -0700

    10: initial files

---
+++ 10/10.py
@@ -0,0 +1,40 @@
+import sys
+import typing as ty
+from dataclasses import dataclass, field
+from enum import Enum
+import collections
+import re
+import itertools
+from itertools import (
+    islice,
+    pairwise,
+    zip_longest
+)
+from functools import (
+    partial,
+    reduce,
+)
+import numpy as np
+from pprint import pprint
+
+from tools import *
+
+def solve1(sections: list[list[str]]) -> int:
+    return -1
+
+def solve2(sections: list[list[str]]) -> int:
+    return -1
+
+def part1(fname: str):
+    with open(fname) as f:
+        sections = read_sections(f)
+    print(f'*** part 1 ***', solve1(sections))
+    
+def part2(fname: str):
+    with open(fname) as f:
+        sections = read_sections(f)
+    print(f'*** part 2 ***', solve2(sections))
+
+if __name__ == '__main__':
+    part1(sys.argv[1])
+    part2(sys.argv[1])
