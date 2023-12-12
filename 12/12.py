61d000bfaa18dc0024c34f659febe8e10769f1cb
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-11 23:23:02 -0700

    12: solution for part 1

--- 12/12.py
+++ 12/12.py
@@ -19,8 +19,51 @@ from pprint import pprint
 
 from tools import *
 
+
 def solve1(sections: list[list[str]]) -> int:
-    pass
+    rows = []
+    for i, line in enumerate(sections[0]):
+        pattern, right = line.split()
+        groups = list(map(int, right.split(",")))
+        qpositions = [
+            m.start()
+            for m in
+            re.finditer(r'\?', pattern)
+        ]
+        assert len(qpositions) > 0, f'line {i}: no ? marks'
+        rows.append((qpositions, pattern, groups))
+        
+    def replace(orig: list[str], replacements) -> list[str]:
+        new = orig.copy()
+        for pos, newchar in replacements:
+            new[pos] = newchar
+        return new
+    
+    broken = re.compile(r'#+')
+    def matches_groups(groups, s: str) -> bool:
+        lengths = list(map(len, broken.findall(s)))
+        #print('> matches_groups:', s, groups, lengths)
+        return lengths == groups        
+    
+    total = 0
+    for i, (positions, pat, groups) in enumerate(rows):
+        exploded = list(pat)
+                
+        s = itertools.product("#.", repeat=len(positions))
+        s = map(partial(zip, positions), s)
+        s = map(tuple, s)
+        #s = observe(partial(print, '#1'), s)
+        s = map(partial(replace, exploded), s)
+        s = map(partial(''.join), s)
+        #s = observe(partial(print, '#2'), s)
+        s = map(partial(matches_groups, groups), s)
+        count = sum(s)
+        total += count
+        
+        print(f'{i}: {count = } {pat = } {groups = }')
+    
+    return total
+
 
 def solve2(sections: list[list[str]]) -> int:
     pass
@@ -33,7 +76,7 @@ def part1(fname: str):
 def part2(fname: str):
     with open(fname) as f:
         sections = read_sections(f)
-    print(f'*** part 2 ***', solve1(sections))
+    print(f'*** part 2 ***', solve2(sections))
 
 if __name__ == '__main__':
     part1(sys.argv[1])
