8ca4ddde8b43ab9835de2d80954f025564d06ee4
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-12 19:09:38 -0700

    12: backtracking solution (too slow)
    This is non-memoized backtracking solution that accumulates an
    assignment of the '?' characters as it descents. It's structured as an
    generator, which is nice in many way. It solves part 1, and faster
    than the previous 2^n code, but it's still clearly exponential and
    the 5-fold expansion in part 2 makes it fail.
    
    I *believe* the written algorithm is correct, but the recursion is more
    complicated than it seems it should be.
    
    Also, the search for actual *assignments* that create a solution instead
    of just the *count* of solutions (actually needed by the problem) is
    not help. It also adds various high constant overheads, notably cloning
    the assignment dict when a complete search is found, and there are
    various other small optimizations that can be contemplated.
    
    However, none of them seem likely to remove the fundamental backtracking
    nature of the solution

--- 12/12.py
+++ 12/12.py
@@ -16,13 +16,16 @@ from functools import (
 )
 import numpy as np
 from pprint import pprint
+import contextlib
+import time
+import operator
 
 from tools import *
 
-
-def solve1(sections: list[list[str]]) -> int:
+def parse_rows(lines: list[str], part2=False):
     rows = []
-    for i, line in enumerate(sections[0]):
+
+    for i, line in enumerate(lines):
         pattern, right = line.split()
         groups = list(map(int, right.split(",")))
         qpositions = [
@@ -30,44 +33,145 @@ def solve1(sections: list[list[str]]) -> int:
             for m in
             re.finditer(r'\?', pattern)
         ]
-        assert len(qpositions) > 0, f'line {i}: no ? marks'
+        if not part2:
+            assert len(qpositions) > 0, f'line {i}: no ? marks'
         rows.append((qpositions, pattern, groups))
-        
-    def replace(orig: list[str], replacements) -> list[str]:
-        new = orig.copy()
-        for pos, newchar in replacements:
-            new[pos] = newchar
-        return new
-    
-    broken = re.compile(r'#+')
-    def matches_groups(groups, s: str) -> bool:
-        lengths = list(map(len, broken.findall(s)))
-        #print('> matches_groups:', s, groups, lengths)
-        return lengths == groups        
+
+    return rows
+
+def solve1(sections: list[list[str]]) -> int:
+    rows = parse_rows(sections[0])
     
     total = 0
-    for i, (positions, pat, groups) in enumerate(rows):
-        exploded = list(pat)
-                
-        s = itertools.product("#.", repeat=len(positions))
-        s = map(partial(zip, positions), s)
-        s = map(tuple, s)
-        #s = observe(partial(print, '#1'), s)
-        s = map(partial(replace, exploded), s)
-        s = map(partial(''.join), s)
-        #s = observe(partial(print, '#2'), s)
-        s = map(partial(matches_groups, groups), s)
-        count = sum(s)
+    for i, (positions, s, groups) in enumerate(rows):
+        print(f'{i}: {s = } {groups = }')
+        count = 0
+        for assignment in backtrack(s, {}, 0, groups, 0):
+            #print(f'   > {assignment = }') 
+            count += 1
+        print(f'>   {count = }')
         total += count
-        
-        print(f'{i}: {count = } {pat = } {groups = }')
     
     return total
 
+@contextlib.contextmanager
+def update(assignment, key, val):
+    assert key not in assignment
+    assignment[key] = val
+    try:
+        yield
+    finally:
+        del assignment[key]
 
-def solve2(sections: list[list[str]]) -> int:
-    pass
+def backtrack(s, assignment, runlen, lengths, pos):
+    assert 0 <= pos <= len(s)    
+
+    ### base case: we've hit the end of the pattern string
     
+    if pos == len(s) and len(lengths) == 0:
+        yield assignment.copy()
+        return
+    if pos == len(s) and len(lengths) == 1:
+        if runlen == lengths[0]: yield assignment.copy()
+        return
+    if pos == len(s) and len(lengths) > 1:
+        return
+
+    assert 0 <= pos < len(s)
+    assert s[pos] in ('.', '?', '#')
+    
+    ### we've run out of lengths
+
+    if len(lengths) == 0 and s[pos] == '.':
+        yield from backtrack(s, assignment, 0, lengths, pos + 1)
+        return
+    if len(lengths) == 0 and s[pos] == '?':
+        with update(assignment, pos, '.'):
+            yield from backtrack(s, assignment, 0, lengths, pos + 1)
+        return
+    if len(lengths) == 0 and s[pos] == '#':
+        return
+
+    assert len(lengths) > 0
+    nextlen = lengths[0]
+    assert nextlen > 0
+    assert runlen <= nextlen
+    
+    ### still looking for a run
+
+    if runlen == 0 and s[pos] == '.':
+        # no run to start, so advance
+        yield from backtrack(s, assignment, 0, lengths, pos + 1)
+        return
+    if runlen == 0 and s[pos] == '#':
+        # must force a run to begin
+        yield from backtrack(s, assignment, 1, lengths, pos + 1)
+        return
+    if runlen == 0 and s[pos] == '?':
+        # no run so far, try both alternatives
+        with update(assignment, pos, '.'):
+            yield from backtrack(s, assignment, 0, lengths, pos + 1)        
+        with update(assignment, pos, '#'):
+            yield from backtrack(s, assignment, 1, lengths, pos + 1)
+        return
+        
+    ### run is short of curent expected length
+    
+    if runlen < nextlen and s[pos] == '.':
+        # run ends too soon, abandon search
+        return
+    if runlen < nextlen and s[pos] == '#':
+        # required to extend run, continue search
+        yield from backtrack(s, assignment, runlen + 1, lengths, pos + 1)
+        return
+    if runlen < nextlen and s[pos] == '?':
+        # must force ? to '#' to continue the run
+        with update(assignment, pos, '#'):
+            yield from backtrack(s, assignment, runlen + 1, lengths, pos + 1)
+        return
+
+    ### run has hit the current expected length
+    
+    if runlen == nextlen and s[pos] == '.':
+        # consume expected length, keep looking
+        yield from backtrack(s, assignment, 0, lengths[1:], pos + 1)
+        return
+    if runlen == nextlen and s[pos] == '#':
+        # run is now long, abandon this search
+        return
+    if runlen == nextlen and s[pos] == '?':
+        # have to end the run, so assign '.' to pos
+        with update(assignment, pos, '.'):
+            yield from backtrack(s, assignment, 0, lengths[1:], pos + 1) 
+        return
+    
+    assert False, "Not expected to get here!"
+
+def solve2(sections: list[list[str]], expand=5) -> int:
+    rows = parse_rows(sections[0], part2=True)
+    
+    total = 0
+    start_time = lap_start = time.time()
+    for i, (positions, s, groups) in enumerate(rows):
+        start = time.time()
+        new_s = '?'.join(s for _ in range(expand))
+        new_groups = groups * expand
+        print(f'{i}: {s = } {groups = } {expand = }')
+        count = 0
+        for assignment in backtrack(new_s, {}, 0, new_groups, 0):
+            #print(f'   > {assignment = }')
+            count += 1
+        
+        elapsed_time = int(time.time() - start_time)
+        t = time.time()
+        lap_time = int(t - lap_start)
+        lap_start = t
+        print(f'>   {count = }')
+        print(f'>   {lap_time = } {elapsed_time = }')
+        total += count
+    
+    return total
+
 def part1(fname: str):
     with open(fname) as f:
         sections = read_sections(f)
@@ -78,6 +182,36 @@ def part2(fname: str):
         sections = read_sections(f)
     print(f'*** part 2 ***', solve2(sections))
 
+def usage(message):
+    prog = sys.argv[0]
+    print(f'usage: {prog} [...] input_file')
+    print(f'    {message}')
+    sys.exit(0)
+
+def main(args):
+    infile = None
+    run1 = run2 = False
+    
+    while args and args[0].startswith('-'):
+        arg = args.pop(0)
+        if arg in ('--'): break
+        elif re.match(r'^-[A-Za-z0-9]{2,}$', arg):
+            args[:0] = list(map(partial(operator.add, '-'), arg[1:]))
+        elif arg in ('-1'): run1 = True
+        elif arg in ('-2'): run2 = True
+
+    if not (run1 or run2): run1 = run2 = True
+
+    if len(args) == 0:
+        usage("missing input file")
+        
+    if run1:
+        for infile in args:
+            part1(infile)
+
+    if run2:
+        for infile in args:
+            part2(infile)
+    
 if __name__ == '__main__':
-    part1(sys.argv[1])
-    part2(sys.argv[1])
+    main(sys.argv[1:])
