cd9199a71917b6533a737a91fe52fb7e79bf6ed5
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-15 00:05:57 -0700

    16: initial files

---
+++ 16/16.py
@@ -0,0 +1,76 @@
+import sys
+import typing as ty
+from dataclasses import dataclass, field
+from enum import Enum
+import collections
+import re
+import itertools
+from itertools import (
+    islice,
+    pairwise,
+    zip_longest
+)
+from functools import (
+    partial,
+    reduce,
+    lru_cache,
+)
+import numpy as np
+from pprint import pprint
+import contextlib
+import time
+import operator
+
+from tools import *
+
+def solve1(sections: list[list[str]]) -> int:
+    return -1
+
+def solve2(sections: list[list[str]]) -> int:
+    return -1
+    
+def part1(fname: str):
+    with open(fname) as f:
+        sections = read_sections(f)
+    print(f'*** part 1 ***', solve1(sections))
+    
+def part2(fname: str):
+    with open(fname) as f:
+        sections = read_sections(f)
+    print(f'*** part 2 ***', solve2(sections))
+
+def usage(message):
+    prog = sys.argv[0]
+    print(f'usage: {prog} [-1|-2] [--] input_file...')
+    print(f'    {message}')
+    sys.exit(1)
+
+def main(args):
+    infile = None
+    run1 = run2 = False
+    
+    while args and args[0].startswith('-'):
+        arg = args.pop(0)
+        if arg in ('--'): break
+        elif re.match(r'^-[A-Za-z0-9]{2,}$', arg):
+            args[:0] = list(map(partial(operator.add, '-'), arg[1:]))
+        elif arg in ('-1'): run1 = True
+        elif arg in ('-2'): run2 = True
+        else:
+            usage(f'{arg}: unexpected option')
+
+    if not (run1 or run2): run1 = run2 = True
+
+    if len(args) == 0:
+        usage("missing input file")
+        
+    if run1:
+        for infile in args:
+            part1(infile)
+
+    if run2:
+        for infile in args:
+            part2(infile)
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])
