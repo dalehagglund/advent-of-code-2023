26d9379e266b01de7f8192d4630d86c269c1e65e
Dale Hagglund <dale.hagglund@gmail.com>
2023-12-16 00:24:24 -0700

    16: solution for part 1

--- 16/16.py
+++ 16/16.py
@@ -23,8 +23,104 @@ import operator
 
 from tools import *
 
+def show_grid(grid: np.array, indent: int = 4):
+    nrows, ncols = grid.shape
+    prefix = " " * indent
+    for line in map(''.join, (list(grid[r, :]) for r in range(nrows))):
+        print(f'{prefix}{line}')
+
+Point = tuple[int, int]
+
+class Point(ty.NamedTuple):
+    r: int
+    c: int
+    
+    def __add__(self, other):
+        iter(self)
+        iter(other)
+        r, c = map(operator.add, self, other)
+        return Point(r, c)
+
+class Dir:
+    U = (-1,  0)
+    D = (+1,  0)
+    L = ( 0, -1)
+    R = ( 0, +1)
+    
+pass_through_dirs = {
+    '-': (Dir.L, Dir.R),
+    '|': (Dir.U, Dir.D),
+}
+
+split = {
+    ('-', Dir.U): (Dir.L, Dir.R),
+    ('-', Dir.D): (Dir.L, Dir.R),
+    ('-', Dir.L): (Dir.L,),
+    ('-', Dir.R): (Dir.R,),
+
+    ('|', Dir.U): (Dir.U,),
+    ('|', Dir.D): (Dir.D,),
+    ('|', Dir.L): (Dir.U, Dir.D),
+    ('|', Dir.R): (Dir.U, Dir.D),
+}
+
+reflect = {
+    ('/',  Dir.U): Dir.R,
+    ('/',  Dir.D): Dir.L,
+    ('/',  Dir.L): Dir.D,
+    ('/',  Dir.R): Dir.U,
+
+    ('\\', Dir.U): Dir.L,
+    ('\\', Dir.D): Dir.R,
+    ('\\', Dir.L): Dir.U,
+    ('\\', Dir.R): Dir.D,
+}
+
+def follow_beam(grid, startpos, startdir):
+    nrows, ncols = grid.shape
+    
+    queue = collections.deque()
+    seen = set()
+    queue.appendleft((startpos, startdir))
+    while len(queue) > 0:
+        curpos, curdir = queue.popleft()
+        if (curpos, curdir) in seen:
+            continue
+        seen.add((curpos, curdir))
+        r, c = curpos
+        in_bounds = (0 <= r < nrows and 0 <= c < ncols)
+        if not in_bounds:
+            continue
+
+        yield curpos, curdir    
+
+        at_pos = grid[curpos]
+        if at_pos == '.':
+            queue.appendleft((curpos + curdir, curdir))
+        elif at_pos in '''-|''':
+            for nextdir in split[(at_pos, curdir)]:
+                queue.appendleft((curpos + nextdir, nextdir))
+        elif at_pos in '''\/''':
+            nextdir = reflect[(at_pos, curdir)] 
+            queue.appendleft((curpos + nextdir, nextdir))
+        else:
+            assert False, f"unexpected char at {curpos = }: {at_pos = }"
+    
+def make_grid(lines: list[str]) -> np.array:
+    return np.array(list(map(list, lines)))
+
 def solve1(sections: list[list[str]]) -> int:
-    return -1
+    grid = make_grid(sections[0])
+    show_grid(grid)
+    counts = np.zeros(grid.shape)
+
+    for point, dir in follow_beam(grid, Point(0, 0), Dir.R):
+        #print(point, dir)
+        counts[point] += 1
+        
+    print(counts)
+    
+    return np.sum(counts > 0)
 
 def solve2(sections: list[list[str]]) -> int:
     return -1
